f0e6df8bbcdab46b177cf7d0e2bf07c847bcb73f
Shivam H Patel <67276090+shivampat@users.noreply.github.com>
2026-06-19 15:39:48 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # [5, 1, 2, 10] 
+        n = len(nums) 
+        if n <= 1:
+            return nums[0]
+        
+        two, one = nums[0], max(nums[1], nums[0])
+
+        for i in range(2, n):
+            tmp = one
+            new = max(two + nums[i], one)
+            two, one = tmp, new
+        
+        return one
+        
+
